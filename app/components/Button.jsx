56df618de20e8a0bde410f5c662dccafe7e7d46d
Felipe Lima <pegasusyt1@gmail.com>
2024-09-11 14:16:28 -0300

    finalizado

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Button = () => {
   return (
-    <div className="flex justify-center items-center mt-12 mb-12">
+    <div className="flex flex-col justify-center items-center mt-12 mb-12">
       <a href="https://wa.me/message/YNQCDX67BOR6N1">
         <button className="flex justify-center items-center bg-[#A4D434] text-[#572d15] font-bold py-6 px-32 rounded-xl shadow-green hover:text-white">
           Quero Adquirir!
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,35 +4,48 @@ import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer class="bg-[#be703f]">
-      <div class="mx-auto max-w-7xl py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+    <footer className="bg-[#be703f]">
+      <div className="mx-auto max-w-7xl py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <nav
-          class="-mx-5 -my-2 flex flex-wrap justify-center order-2"
+          className="-mx-5 -my-2 flex flex-wrap justify-center order-2"
           aria-label="Footer"
         >
-          <div class="px-5">
-            <a href="#" class="text-base text-[#c5ff3f] hover:text-[#add64b]">
+          <div className="px-5">
+            <a
+              href="#"
+              className="text-base text-[#c5ff3f] hover:text-[#add64b]"
+            >
               Todos direitos reservados.
             </a>
           </div>
         </nav>
-        <div class="mt-8 md:mb-8 flex justify-center space-x-6 md:order-3  ">
-          <a
-            href="https://wa.me/message/YNQCDX67BOR6N1"
-            class="text-[#c5ff3f] hover:text-[#add64b]"
-          >
-            <FaWhatsapp />
-          </a>
-
-          <a
-            href="https://www.instagram.com/mrtipsbr/"
-            class="text-[#c5ff3f] hover:text-[#add64b]"
-          >
-            <FaInstagram />
-          </a>
+        <div className="mt-8 md:mb-8 flex justify-center space-x-6 md:order-3">
+          <div className="flex flex-row flex-nowrap items-center">
+            <a
+              href="https://wa.me/message/YNQCDX67BOR6N1"
+              className="text-[#c5ff3f] hover:text-[#add64b] flex items-center"
+            >
+              <FaWhatsapp className="text-2xl" />{" "}
+              {/* Ensuring consistent icon size */}
+              <h3 className="text-md ml-2">(61) 99817-2801</h3>{" "}
+              {/* Set consistent font size */}
+            </a>
+          </div>
+          <div className="flex flex-row flex-nowrap items-center">
+            <a
+              href="https://www.instagram.com/mrtipsbr/"
+              className="text-[#c5ff3f] hover:text-[#add64b] flex items-center"
+            >
+              <FaInstagram className="text-2xl" />{" "}
+              {/* Ensuring consistent icon size */}
+              <h3 className="text-md ml-2">@mrtipsbr</h3>{" "}
+              {/* Set consistent font size */}
+            </a>
+          </div>
         </div>
-        <div class="mt-8  md:order-1 md:mt-0 ">
-          <p class="text-center text-base text-[#c5ff3f]">
+
+        <div className="mt-8  md:order-1 md:mt-0">
+          <p className="text-center text-base text-[#c5ff3f]">
             &copy; Mr. Tips Website.
           </p>
         </div>
