9ff6008214ca69402167d33f13662fc3e538a305
kirz4 <lucasrabello098@gmail.com>
2024-09-23 19:14:31 -0300

    video

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import MrTips from "./assets/MrTips.mp4";
+import MrTips from "../../public/images/MrTips.mp4";
 
 const HeroSection = () => {
   const videoRef = useRef(null);
